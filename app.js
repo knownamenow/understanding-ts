de1b3967bf9f0a5f07e41e2aaa56103cb85b404b
anawat wannapila <knownamenow@gmail.com>
2023-03-05 01:15:08 +0700

    type in ts

---
+++ app.js
@@ -0,0 +1,15 @@
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code
+    };
+}
+var result = generateError('An error occurred!', 500);
+console.log('🚨 ~ result:', result);
--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log('ts: ')
+let userInput: unknown
+let userName: string
+userInput = 5
+userInput = 'Max'
+
+if (typeof userInput === 'string') {
+  userName = userInput
+}
+
+function generateError(message: string, code: number): never {
+  throw {
+    message,
+    errorCode: code
+  }
+}
+
+generateError('An error occurred!', 500)
