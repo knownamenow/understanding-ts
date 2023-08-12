48e8d131cd42735c965c7d93047aa693f0c668dd
anawat wannapila <17815326+knownamenow@users.noreply.github.com>
2023-08-13 00:53:09 +0700

    feat: deafult function parameters

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ age = 29
 //   return result
 // }
 
-const add = (a: number, b: number) => a + b
+const add = (a: number, b: number = 1) => a + b
 
 console.log(add(5, 2))
 
@@ -17,7 +17,7 @@ const printOutput: (a: string | number) => void = output => console.log(output)
 const button = document.querySelector('button')
 
 if (button) {
-  button.addEventListener('click', event => console.log(event))
+  button.addEventListener('click', console.log)
 }
 
-printOutput(add(5, 2))
+printOutput(add(5))
